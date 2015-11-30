d8a1498d82f7a4e79beb0313315ab9ff7df07317
Max Mulatz <klappradla@gmail.com>
2015-12-01 00:59:37 +0100

    update transfer sh functions

--- bin/transfer.sh
+++ bin/transfer.sh
@@ -2,11 +2,21 @@
 
 # variables
 SOURCE_DIR=~/Code
-BACKUP_DIR=max@HerrDirektor.local:'~'
-EXCLUDES='.DS_STORE'
+REMOTE_DIR=max@HerrDirektor.local
+EXCLUDE_LIST=~/dotfiles/bin/backup-excludes.txt
 
 # run backup task
 read -p "Ready to transfer files now? (y/n) " RESP
 if [ "$RESP" = "y" ]; then
-  rsync -av --delete --exclude $EXCLUDES $SOURCE_DIR -e ssh $BACKUP_DIR
+  rsync -av --delete --progress --exclude-from $EXCLUDE_LIST $SOURCE_DIR -e ssh "$REMOTE_DIR":"~"
+
+  echo "Transfer Music..."
+  rsync -av --delete --progress --exclude-from $EXCLUDE_LIST \
+  "/Volumes/Media/iTunes Media/Music" \
+  -e ssh "$REMOTE_DIR":"~/Music/iTunes/iTunes\ Media/"
+
+  echo "Transfer Library..."
+  rsync -av --delete --progress --exclude-from $EXCLUDE_LIST \
+  "/Volumes/Media/iTunes Library.itl" \
+  "$REMOTE_DIR":"~/Music/iTunes/"
 fi
