d8b3c7d4847bb9d1b15f83cf31eeb5d480321ce1
Max Mulatz <klappradla@gmail.com>
2016-08-23 00:07:40 +0200

    WIP new backup solution

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,38 +1,67 @@
 #!/bin/bash
 
-readonly LOG_FILE=~/dotfiles/scripts/log/backup.log
+# readonly LOG_FILE=~/dotfiles/scripts/log/backup.log
 readonly EXCLUDE_LIST=~/dotfiles/scripts/rsync-excludes.txt
-readonly SOURCE_DIRS=(~/Code
-                      ~/Dropbox
-                      /Volumes/Media/iTunes\ Media/Music)
-readonly BACKUP_DRIVE="/Volumes/archive"
-readonly BACKUP_VOLUME="/Volumes/archive/backup/daily/$(date +'%A')"
-readonly REMOTE=max@HerrDirektor.local
+# readonly SOURCE_DIRS=(~/Code
+#                       ~/Dropbox
+#                       /Volumes/Media/iTunes\ Media/Music)
 
-print_to_log() {
-  local txt=$1
-  echo "$1" >> $LOG_FILE
+readonly SOURCE_DIRS=(~/Desktop/source)
+# readonly BACKUP_DRIVE="/Volumes/archive"
+readonly BACKUP_DRIVE=~/Desktop/tryout
+# readonly BACKUP_VOLUME="/Volumes/archive/backup/daily/$(date +'%A')"
+readonly BACKUP_VOLUME=~/Desktop/goal
+# readonly REMOTE=max@HerrDirektor.local
+
+# print_to_log() {
+#   local txt=$1
+#   echo "$1" >> $LOG_FILE
+# }
+
+backup_ran() {
+  echo "check for file"
+  local date="$(date +'%F')"
+  local run_file="$BACKUP_VOLUME/.last_run"
+  [ -e "$run_file" ] && [ grep -Fxq "$date" $run_file ]
 }
 
-remote_backup() {
-  local dir
-  print_to_log "\nTry remote backup $(date +%Y_%m_%d_%H:%M:%S)"
-  for dir in "${SOURCE_DIRS[@]}"
-  do
-    sync_to_remote "$dir"
-  done
+menu_bar() {
+  local color=$1
+  echo -n "$color" | nc -4u -w0 localhost 1738
 }
 
-sync_to_remote() {
-  local src=$1
-  print_to_log "Transfer $src..."
-  rsync -av --delete --exclude-from $EXCLUDE_LIST \
-    "$src" -e ssh "$REMOTE":"$BACKUP_VOLUME" >> $LOG_FILE
+set_status() {
+  local status=$1
+  if [ "$status" -eq "0" ]
+  then
+    menu_bar green
+  else
+    menu_bar exclamation
+  fi
 }
+
+# remote_backup() {
+#   local dir
+#   # print_to_log "\nTry remote backup $(date +%Y_%m_%d_%H:%M:%S)"
+#   for dir in "${SOURCE_DIRS[@]}"
+#   do
+#     sync_to_remote "$dir"
+#   done
+# }
+
+# sync_to_remote() {
+#   local src=$1
+#   print_to_log "Transfer $src..."
+#   rsync -av --delete --exclude-from $EXCLUDE_LIST \
+#     "$src" -e ssh "$REMOTE":"$BACKUP_VOLUME" >> $LOG_FILE
+# }
 
 local_backup() {
-  local dir
-  print_to_log "\nLocal backup $(date +%Y_%m_%d_%H:%M:%S)"
+  echo "backup"
+  # local dir
+  # print_to_log "\nLocal backup $(date +%Y_%m_%d_%H:%M:%S)"
+  # if backup_ran_today
+  # then
   for dir in "${SOURCE_DIRS[@]}"
   do
     sync_local "$dir"
@@ -41,28 +70,27 @@ local_backup() {
 
 sync_local() {
   local src=$1
-  print_to_log "Transfer $src..."
-  rsync -av --delete --exclude-from $EXCLUDE_LIST \
-    "$src" $BACKUP_VOLUME >> $LOG_FILE
+  # print_to_log "Transfer $src..."
+  rsync -av --delete --exclude-from $EXCLUDE_LIST "$src" $BACKUP_VOLUME
 }
 
-calculate_log_size() {
-  ( wc $LOG_FILE | awk '{ print $1 }' )
-}
+# calculate_log_size() {
+#   ( wc $LOG_FILE | awk '{ print $1 }' )
+# }
 
-log_too_big() {
-  local size=$1
-  local max_size=6000
-  [ $size -ge $max_size ]
-}
+# log_too_big() {
+#   local size=$1
+#   local max_size=6000
+#   [ $size -ge $max_size ]
+# }
 
-truncate_log() {
-  local size=$1
-  local min_size=300
-  local cut=$(($size-$min_size))
+# truncate_log() {
+#   local size=$1
+#   local min_size=300
+#   local cut=$(($size-$min_size))
 
-  sed -i '' "1,$cut d" $LOG_FILE
-}
+#   sed -i '' "1,$cut d" $LOG_FILE
+# }
 
 drive_connected() {
   local dir=$1
@@ -70,17 +98,20 @@ drive_connected() {
 }
 
 main() {
-  local log_size
+  menu_bar questionmark
+  # local log_size
 
   if drive_connected $BACKUP_DRIVE
   then
-    local_backup
+    (! backup_ran) || local_backup
   else
-    remote_backup
+    # remote_backup
+    echo "remote"
   fi
+  set_status "$?" # here the backup ran sucessfully
 
-  log_size=$(calculate_log_size)
-  log_too_big $log_size \
-    && truncate_log $log_size
+  # log_size=$(calculate_log_size)
+  # log_too_big $log_size \
+  #   && truncate_log $log_size
 }
 main
