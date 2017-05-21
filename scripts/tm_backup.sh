1b21adcea83bd69007c8fd0347c9e2c93e77e969
Max Mulatz <klappradla@gmail.com>
2017-05-21 16:58:01 +0200

    Add script for time machine backup

---
+++ scripts/tm_backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+readonly NAS="monster.local"
+readonly TODAY="$(date +'%F')"
+readonly LAST_RUN_FILE=~/dotfiles/scripts/.last_tm_backup.txt
+
+backup_ran() {
+  [ -e $LAST_RUN_FILE ] &&
+    grep -Fxq $TODAY $LAST_RUN_FILE
+}
+
+tm_backup() {
+  nas_available &&
+  tmutil startbackup &&
+  write_log
+}
+
+write_log() {
+  echo $TODAY > $LAST_RUN_FILE
+}
+
+nas_available() {
+  nc -z $NAS 22 > /dev/null
+}
+
+main() {
+  backup_ran || tm_backup
+}
+main
