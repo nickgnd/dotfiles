9134c213c3300adac6c3d8c1650cec9e088b4d88
Max Mulatz <klappradla@gmail.com>
2017-12-06 12:43:33 +0100

    Move atom init file to javascript
    Also: move to es6 syntax

---
+++ atom/init.js
@@ -0,0 +1,35 @@
+// Your init script
+//
+// Atom will evaluate this file each time a new window is opened. It is run
+// after packages are loaded/activated and after the previous editor state
+// has been restored.
+//
+// An example hack to log to the console when each text editor is saved.
+//
+// atom.workspace.observeTextEditors(function(editor) {
+//   return editor.onDidSave(function() {
+//     return console.log(`Saved! ${editor.getPath()}`);
+//   });
+// });
+
+// Save list of currently installed packages
+atom.commands.add('atom-workspace', 'package-list:dump', () => {
+  const exec = require('child_process').exec;
+  const file = `${process.env['DOTS']}/atom/packages.txt`;
+  const cmd = `apm list --installed --bare > ${file}`;
+  return exec(cmd);
+});
+
+// Install list of packages
+atom.commands.add('atom-workspace', 'package-list:install', () => {
+  const exec = require('child_process').exec;
+  const file = `${process.env['DOTS']}/atom/packages.txt`;
+  const cmd = `apm install --packages-file ${file}`;
+  return exec(cmd);
+});
+
+// Remap annoying alt space to space
+atom.commands.add('atom-text-editor', 'custom:space', () => {
+  const editor = atom.workspace.getActiveTextEditor();
+  return editor.insertText(' ');
+});
