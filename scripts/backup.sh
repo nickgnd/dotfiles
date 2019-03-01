74eb97af113ac7783fd390689a604a964d625152
Max Mulatz <klappradla@posteo.net>
2019-03-01 18:37:28 +0100

    Update rsync options
    Rethink the rsync and ssh options and document them.
    
    closes #2

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,6 +7,7 @@ readonly NAS=$NAS_ADDRESS
 readonly BACKUP_VOLUME="$NET_BACKUP/$(hostname -s)/daily/$(date +'%A')"
 readonly TODAY="$(date +'%F')"
 readonly LAST_RUN_FILE=~/dotfiles/scripts/.last_backup.txt
+readonly CIPHERS=chacha20-poly1305@openssh.com,aes128-gcm@openssh.com
 
 backup_ran() {
   [ -e $LAST_RUN_FILE ] &&
@@ -34,12 +35,21 @@ remote_backup() {
   write_log
 }
 
+# rsync options explained:
+# a -> archive mode (sets some convenient flags)
+# E -> preserve executabiliy
+# X -> preserve extended attributes
+# q -> quite mode (since running in background anyways)
+#
+# ssh options explained:
+# T                -> no pseudo tty (less CPU load on destination)
+# o Compression=no -> no compression (faster)
+# c <ciphers>      -> use a weaker, but faster openssl cipher
 sync_files() {
   menu_bar orange
   for dir in "${SOURCE_DIRS[@]}"; do
-    rsync -atvz --delete --exclude-from \
-      $EXCLUDE_LIST \
-      -e "ssh -T -c aes128-gcm@openssh.com -o compression=no -x"\
+    rsync -aEXq --delete --exclude-from $EXCLUDE_LIST \
+      -e "ssh -T -c $CIPHERS -o Compression=no -x"\
       "$dir" max@"$NAS":"$BACKUP_VOLUME"
   done
 }
