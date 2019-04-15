05d826a5bb484c95ee833788e08d44b121a61a57
nickgnd <nicolo.gnudi@gmail.com>
2019-04-15 23:23:43 +0200

    Load `oh-my-zsh` for real
    - Rename `$ZSH` to `$ZSHDOT` in .dotfiles
    - Use `$ZSH` for the `.oh-my-zsh` folder

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -3,9 +3,16 @@
 # Oh-my-zsh
 #
 # This installs oh-my-zsh (https://github.com/robbyrussell/oh-my-zsh).
-
+#
 export ZSH=~/.oh-my-zsh
 
 if [ ! -d $ZSH ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+# This installs zsh syntax highlighting
+#
+ZSH_SYNTAX_HIGHLIGHTING_FOLDER=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+if [ -d $ZSH ] && [ ! -d "$ZSH_SYNTAX_HIGHLIGHTING_FOLDER" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_SYNTAX_HIGHLIGHTING_FOLDER
 fi
