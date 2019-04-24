51f945b9c83bc8b62e78bfe87de7d2b6f9fc0cc8
nickgnd <nicolo.gnudi@gmail.com>
2019-04-24 09:19:58 +0200

    Installing asdf version manager
    - Remove `brew yarn` because doesn't play nicely
      with `asdf-vm`
    - Don't use the `asdf` plugin from `oh-my-zsh`
      to have more control paths loading order
    - Install node, ruby, erlang and elixir plugins
    - Symlink `tool-versions` to the user root
    - Allow legacy versions format

---
+++ asdf/env.zsh
@@ -0,0 +1,1 @@
+export ASDF_DIR="${HOME}/.asdf"
---
+++ asdf/install.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+#
+# asdf - Extendable version manager with support for Ruby, Node.js, Elixir, Erlang & more 
+#
+# This installs asdf (https://github.com/asdf-vm/asdf), then be sure to enable the `asdf` plugin on `oh-my-zsh`.
+#
+
+if [ ! -f $ASDF_DIR/asdf.sh ]; then
+  echo 'installing asdf-vm...'
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.1
+fi
+
+# Source asdf before install the plugins
+source $HOME/.asdf/asdf.sh
+
+
+## Install plugins
+
+# installed_plugins=$(asdf plugin-list)
+asdf_detect_plugin() {
+  asdf plugin-list | grep -q ${1}
+  return $?
+}
+
+# Erlang
+if asdf_detect_plugin 'erlang'; then
+  echo 'asdf - erlang plugin already installed'
+else
+  echo 'asdf - installing the erlang plugin'
+  asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
+fi
+
+
+# Elixir
+if asdf_detect_plugin 'elixir'; then
+  echo 'asdf - elixir plugin already installed'
+else
+  echo 'asdf - installing the elixir plugin'
+  asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+fi
+
+# Node
+if asdf_detect_plugin 'node'; then
+  echo 'asdf - node plugin already installed'
+else
+  echo 'asdf - installing the node plugin'
+  asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+  bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+fi
+
+# Ruby
+if asdf_detect_plugin 'ruby'; then
+  echo 'asdf - ruby plugin already installed'
+else
+  echo 'asdf - installing the ruby plugin'
+  asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+fi
