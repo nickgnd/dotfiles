249d6943dcc6129edf54e041c1f1338fc6564cc3
Max Mulatz <klappradla@posteo.net>
2019-06-26 19:29:21 +0200

    Add some more mac defaults

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -19,12 +19,18 @@ defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
 # use column view by default
 defaults write com.apple.Finder FXPreferredViewStyle icnv
 
+# don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
 ####
 # Dock
 ####
 
 # show and hide Dock
 defaults write com.apple.dock autohide -bool true
+
+# don’t show recent applications in Dock
+defaults write com.apple.dock show-recents -bool false
 
 ####
 # Keyboard
@@ -35,8 +41,27 @@ defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
 
 ####
+# Touchpad
+####
+
+# enable tab to click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write -g com.apple.mouse.tapBehavior -int 1
+
+# increase scrolling speed of trackpad
+defaults wrtie -g com.apple.trackpad.scaling -int 3
+
+####
+# Mouse
+####
+
+# increase scrolling speed of mouse
+defaults wrtie -g com.apple.mouse.scaling -int 3
+
+####
 # Apps
 ####
+
 # load settings for iTerm from dotfiles
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/dotfiles/iterm2"
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
