28ff36d5f9959eca180f828df2b819fa3b1d5f7a
Max Mulatz <klappradla@posteo.net>
2019-10-28 23:16:27 +0100

    Update asdf script
    Update script to also update all plugins and the core version manager.

--- scripts/asdf.sh
+++ scripts/asdf.sh
@@ -1,5 +1,5 @@
 readonly REPO="https://github.com/asdf-vm/asdf.git"
-readonly VERSION="v0.7.3"
+readonly VERSION="v0.7.4"
 
 . "$HOME/dotfiles/scripts/print.sh"
 
@@ -9,10 +9,11 @@ install_plugin() {
   if ! asdf plugin-list | grep -Fq "$name"; then
     asdf plugin-add "$name" "$url" | print_info
   fi
-  success "asdf plugin $name installed"
+  success "Installed $name asdf plugin"
 }
 
 install_plugins() {
+  info "Installing asdf plugins..."
   install_plugin "ruby" "https://github.com/asdf-vm/asdf-ruby.git"
   install_plugin "nodejs" "https://github.com/asdf-vm/asdf-nodejs.git"
   install_plugin "postgres" "https://github.com/smashedtoatoms/asdf-postgres"
@@ -25,17 +26,28 @@ plugin_setup() {
   "$HOME/.asdf/plugins/nodejs/bin/import-release-team-keyring" 2>&1 | print_info
 }
 
-install_asdf() {
+update_plugins() {
+  info "Updating asdf plugins..."
+  asdf plugin-update --all | print_info
+  success "Updated asdf plugins"
+}
+
+install_or_update_asdf() {
   local path="$HOME/.asdf"
   if [ ! -d "$path" ]; then
     info "Installing asdf"
     git clone "$REPO" "$path" --branch "$VERSION" \
       | print_info
+    success "Installed asdf"
+  else
+    info "Updating asdf..."
+    asdf update | print_info
+    success "Updated asdf"
   fi
   source "$path/asdf.sh"
-  success "asdf installed"
 }
 
-install_asdf \
+install_or_update_asdf \
   && install_plugins \
-  && plugin_setup
+  && plugin_setup \
+  && update_plugins
