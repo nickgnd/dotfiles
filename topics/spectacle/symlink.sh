004bc418b68fe372f652ee88fb2481cec578fdab
Max Mulatz <klappradla@posteo.net>
2020-03-12 22:16:09 +0100

    Add spectacle topic

---
+++ topics/spectacle/symlink.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+source "../../scripts/utils.sh"
+
+readonly TOPIC_DIR=$(pwd)
+# NOTE: necessary for having absolute paths
+readonly SYMLINKS=$(find "$TOPIC_DIR" -maxdepth 1 -name "*.symlink")
+
+for src in $SYMLINKS; do
+  name=$(basename "$src" .symlink)
+  dest="$HOME/Library/Application Support/$name"
+  symlink "$src" "$dest"
+done
+
