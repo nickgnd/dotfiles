5b6a040287072c008eb55893c683e5ae93077b3a
Max Mulatz <klappradla@posteo.net>
2020-03-12 21:58:17 +0100

    Add neovim topic and symlink logic

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,12 +5,21 @@ set -e
 readonly TOPICS=(homebrew zsh mac asdf neovim)
 # readonly TOPICS=(neovim)
 readonly TOPICS_DIR="$(dirname "$0")/../topics"
-# readonly TOPICS_DIR="$HOME/dotfiles/topics"
 
-for topic in "${TOPICS[@]}"; do
-  cd "$TOPICS_DIR/$topic"
+install_topics() {
+  export local OVERWRITE_SYMLINKS=false
+  export local BACKUP_SYMLINKS=false
+  export local SKIP_SYMLINKS=false
 
-  find . -maxdepth 1 -name install.sh \
-    | while read -r installer; do $installer; done
-    # | while read -r installer; do $installer -y; done
-done
+  for topic in "${TOPICS[@]}"; do
+    cd "$TOPICS_DIR/$topic"
+
+    find . -maxdepth 1 -name install.sh \
+      | while read -r installer; do $installer; done
+      # | while read -r installer; do $installer -y; done
+    find . -maxdepth 1 -name symlink.sh \
+      | while read -r symlink; do $symlink; done
+  done
+}
+
+install_topics
---
+++ scripts/utils.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -e
+
+print_success() {
+  local msg=$1
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n" "$msg"
+}
+
+print_info() {
+  local msg=$1
+  printf "\r  [\033[00;34mINFO\033[0m] %s\n" "$msg"
+}
+
+print_question() {
+  local msg=$1
+  printf "\r  [ \033[0;33m??\033[0m ] -> %s\n" "$msg"
+}
+
+print_progress() {
+  local data
+  while read -r data; do
+    printf "\r  [ \033[00;34m..\033[0m ] %s\n" "${data}"
+  done
+}
+
+symlink_present() {
+  local dest=$1
+  # NOTE: checks if dest is a file, directory or symlink
+  [ -f "$dest" ]  || [ -d "$dest" ] || [ -L "$dest" ]
+}
+
+symlink () {
+  local src=$1 dest=$2
+  local overwrite=false backup=false skip=false
+  local action
+
+  if symlink_present "$dest"; then
+    if [ "$OVERWRITE_SYMLINKS" == "false" ] && [ "$BACKUP_SYMLINKS" == "false" ] && [ "$SKIP_SYMLINKS" == "false" ]; then
+
+      if [ "$(readlink "$dest")" == "$src" ]; then
+        print_info "LINK $src TO $dest ALREADY EXISTS…"
+        return 0
+      else
+
+        print_question "File already exists: $dest ($(basename "$src")), what do you want to do?"
+        print_question "[s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
+        read -rn 1 action </dev/tty
+
+        case "$action" in
+          o ) overwrite=true;;
+          O ) overwrite=true OVERWRITE_SYMLINKS=true;;
+          b ) backup=true;;
+          B ) backup=true BACKUP_SYMLINKS=true;;
+          s ) skip=true;;
+          S ) skip=true SKIP_SYMLINKS=true;;
+          * ) ;;
+        esac
+      fi
+    fi
+
+    if [ "$overwrite" == "true" ]; then
+      rm -rf "$dest"
+      print_success "REMOVED $dest"
+    elif [ "$backup" == "true" ]; then
+      mv "$dest" "${dest}.backup"
+      print_success "MOVED $dest TO ${dest}.backup"
+    elif [ "$skip" == "true" ]; then
+      print_success "SKIPPED SYMLINK $src"
+    fi
+  fi
+
+  if [ "$skip" != "true" ]; then
+    ln -s "$src" "$dest"
+    print_success "LINKED $src TO $dest"
+  fi
+}
