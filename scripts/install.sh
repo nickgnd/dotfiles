982747195c4b2559011ad51afe69497b78e00f58
Max Mulatz <klappradla@posteo.net>
2020-03-12 13:46:13 +0100

    Add install / update logic for homebrew

---
+++ scripts/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+readonly TOPICS=(homebrew)
+readonly TOPICS_DIR="$(dirname "$0")/../topics"
+# readonly TOPICS_DIR="$HOME/dotfiles/topics"
+
+for topic in "${TOPICS[@]}"; do
+  cd "$TOPICS_DIR/$topic"
+
+  find . -maxdepth 1 -name install.sh \
+    | while read -r installer; do $installer; done
+done
---
+++ topics/homebrew/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+readonly HOMEBREW_URL="https://raw.githubusercontent.com/Homebrew/install/master/install"
+
+print_info() {
+  local msg=$1
+  printf "\r  [\033[00;34mINFO\033[0m] %s\n" "${msg}"
+}
+
+print_progress() {
+  local data
+  while read -r data; do
+    printf "\r  [ \033[00;34m..\033[0m ] %s\n" "${data}"
+  done
+}
+
+check_dependencies() {
+  brew bundle check | print_progress
+  exit "${PIPESTATUS[0]}" # return exit status of check command
+}
+
+install_or_update_dependencies() {
+  brew bundle --no-lock | print_progress
+}
+
+if ! command -v brew >/dev/null; then
+  print_info "INSTALL HOMEBEW…"
+  curl -fsSL "$HOMEBREW_URL" | ruby 2>&1 | print_progress
+else
+  print_info "UPDATE HOMEBEW…"
+  brew update | print_progress
+fi
+
+print_info "INSTALL/UPDATE BREWS…"
+check_dependencies || install_or_update_dependencies
