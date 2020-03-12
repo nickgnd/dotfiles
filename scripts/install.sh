b454cbd2f44a7555efbdec85ff28334a7bd586bc
Max Mulatz <klappradla@posteo.net>
2020-03-12 23:29:19 +0100

    Move ssh to topic

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-# readonly TOPICS=(homebrew zsh mac asdf neovim karabiner spectacle iterm editorconfig git tmux)
-readonly TOPICS=(zsh)
+# readonly TOPICS=(homebrew zsh mac asdf neovim karabiner spectacle iterm editorconfig git tmux ssh)
+readonly TOPICS=(ssh)
 readonly TOPICS_DIR="$(dirname "$0")/../topics"
 
 install_topics() {
