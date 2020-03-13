8e50330fc2406b5e7e8e349487e2577ea612dfa9
Max Mulatz <klappradla@posteo.net>
2020-03-13 15:16:42 +0100

    Add flag for install script
    Pass the "force" flag when updating to not prompt for the computer name
    again.

--- mac/install.sh
+++ mac/install.sh
@@ -13,9 +13,9 @@ print_question() {
 }
 
 set_name() {
-  local old_name name interactive=$1
+  local old_name name force=$1
   old_name=$(scutil --get ComputerName)
-  if [ "$interactive" == true ]; then
+  if [ "$force" == false ]; then
     print_question "HOW SHOULD THIS COMPUTER BE CALLED? (currently: $old_name)"
     printf -v prompt "\r  [ \033[0;33m!!\033[0m ] -> "
     read -r -e -p "$prompt" name </dev/tty
@@ -27,11 +27,11 @@ set_name() {
   sudo scutil --set LocalHostName "$name"
 }
 
-declare INTERACTIVE
-while getopts "y" option; do
+declare force=false
+while getopts "f" option; do
   case $option in
-    y)  INTERACTIVE=false;;
-    \?) INTERACTIVE=true ;;
+    f)  force=true;;
+    \?) force=false;;
   esac
 done
 
@@ -39,4 +39,4 @@ print_info "SETTING MACOS DEFAULTS…"
 ./defaults.sh
 
 print_info "SETTING COMPUTER NAME…"
-set_name $INTERACTIVE
+set_name "$force"
--- scripts/install.sh
+++ scripts/install.sh
@@ -7,24 +7,33 @@ readonly DIR_PATTERN=(! -name ".git" -and ! -name "homebrew" -and ! -name "scrip
 readonly TOPICS=$(find "$DOTS_DIR" -maxdepth 1 -type d "${DIR_PATTERN[@]}")
 
 install_topic() {
-  local topic=$1
+  local topic=$1 flags=$2
   cd "$topic"
   find . -maxdepth 1 -name install.sh \
-    | while read -r installer; do $installer; done
+    | while read -r installer; do $installer "$flags"; done
   find . -maxdepth 1 -name symlink.sh \
     | while read -r symlink; do $symlink; done
 }
 
 install_topics() {
+  local flags=$1
   export local OVERWRITE_SYMLINKS=false
   export local BACKUP_SYMLINKS=false
   export local SKIP_SYMLINKS=false
 
-  install_topic "$DOTS_DIR/homebrew"
+  install_topic "$DOTS_DIR/homebrew" "$flags"
 
   for topic in ${TOPICS[*]}; do
-    install_topic "$topic"
+    install_topic "$topic" "$flags"
   done
 }
 
-install_topics
+declare flags=""
+while getopts "f" option; do
+  case $option in
+    f)  flags=-f;;
+    \?) flags="";;
+  esac
+done
+
+install_topics $flags
