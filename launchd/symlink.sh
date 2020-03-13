bf8986b7c00289d9557fa5ebb829e4a3e7d962e8
Max Mulatz <klappradla@posteo.net>
2020-03-13 13:05:59 +0100

    Move topics directory to root

---
+++ launchd/symlink.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+source "../scripts/utils.sh"
+
+readonly TOPIC_DIR=$(pwd)
+# NOTE: necessary for having absolute paths
+readonly SYMLINKS=$(find "$TOPIC_DIR" -maxdepth 1 -name "*.symlink")
+
+for src in $SYMLINKS; do
+  name=$(basename "$src" .symlink)
+  dest="$HOME/Library/LaunchAgents//$name"
+  symlink "$src" "$dest"
+done
--- scripts/install.sh
+++ scripts/install.sh
@@ -2,24 +2,28 @@
 
 set -e
 
-# readonly TOPICS=(homebrew zsh mac asdf neovim karabiner spectacle iterm editorconfig git tmux ssh launchd)
-readonly TOPICS=(launchd)
-readonly TOPICS_DIR="$(dirname "$0")/../topics"
+readonly DOTS_DIR="$(dirname "$(dirname "$0")")"
+readonly DIR_PATTERN=(! -name ".git" -and ! -name "homebrew" -and ! -name "scripts")
+readonly TOPICS=$(find "$DOTS_DIR" -maxdepth 1 -type d "${DIR_PATTERN[@]}")
+
+install_topic() {
+  local topic=$1
+  cd "$topic"
+  find . -maxdepth 1 -name install.sh \
+    | while read -r installer; do $installer; done
+  find . -maxdepth 1 -name symlink.sh \
+    | while read -r symlink; do $symlink; done
+}
 
 install_topics() {
   export local OVERWRITE_SYMLINKS=false
   export local BACKUP_SYMLINKS=false
   export local SKIP_SYMLINKS=false
 
-  for topic in "${TOPICS[@]}"; do
-    cd "$TOPICS_DIR/$topic"
+  install_topic "$DOTS_DIR/homebrew"
 
-    find . -maxdepth 1 -name install.sh \
-      | while read -r installer; do $installer; done
-      # | while read -r installer; do $installer -y; done
-    # TODO: use default linking when no special place is necessary...
-    find . -maxdepth 1 -name symlink.sh \
-      | while read -r symlink; do $symlink; done
+  for topic in ${TOPICS[*]}; do
+    install_topic "$topic"
   done
 }
 
