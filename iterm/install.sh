d9b8b548282c2e910a0a0c731a61f087e0768d8b
Max Mulatz <klappradla@posteo.net>
2020-03-13 16:47:46 +0100

    Fix iterm preferences path

--- iterm/install.sh
+++ iterm/install.sh
@@ -3,6 +3,6 @@
 set -e
 
 # load settings for iTerm from dotfiles
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/dotfiles/topics/iterm"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/dotfiles/iterm"
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
