784a5de406a3e186e3aa91e7c86d63a82a6edc21
Max Mulatz <klappradla@posteo.net>
2020-11-28 20:44:42 +0100

    Remove "force" flag from install script
    With extracting the setup script, this is no longer necessary.
    
    Also: add some info output to the script to update submodules.

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,33 +9,24 @@ readonly DIR_PATTERN=(! -name ".git" -and ! -name "homebrew" -and ! -name "scrip
 readonly TOPICS=$(find "$DOTS_DIR" -maxdepth 1 -type d "${DIR_PATTERN[@]}")
 
 install_topic() {
-  local topic=$1 flags=$2
+  local topic=$1
   cd "$topic"
   find . -maxdepth 1 -name install.sh \
-    | while read -r installer; do $installer "$flags"; done
+    | while read -r installer; do $installer; done
   find . -maxdepth 1 -name symlink.sh \
     | while read -r symlink; do $symlink; done
 }
 
 install_topics() {
-  local flags=$1
   export local OVERWRITE_SYMLINKS=false
   export local BACKUP_SYMLINKS=false
   export local SKIP_SYMLINKS=false
 
-  install_topic "$DOTS_DIR/homebrew" "$flags"
+  install_topic "$DOTS_DIR/homebrew"
 
   for topic in ${TOPICS[*]}; do
-    install_topic "$topic" "$flags"
+    install_topic "$topic"
   done
 }
 
-declare flags=""
-while getopts "f" option; do
-  case $option in
-    f)  flags=-f;;
-    \?) flags="";;
-  esac
-done
-
-install_topics "$flags"
+install_topics
--- scripts/update-submodules.sh
+++ scripts/update-submodules.sh
@@ -4,5 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# shellcheck source=./utils.sh
+source "$(dirname "$0")/utils.sh"
+
 cd "$(dirname "$0")/.."
+print_info "UPDATE GIT SUBMODULES…"
 exec git submodule update --remote
