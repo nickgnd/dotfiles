ff4887340d5aff60a872a75a7bbef4996ba4c4db
Max Mulatz <klappradla@posteo.net>
2021-04-30 18:19:42 +0200

    Update helper functions in neovim install script

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# To debug, uncomment:
+# set -o xtrace
 
 source "../script/lib/utils.sh"
 
@@ -8,9 +13,12 @@ readonly VIM_PLUG_URL="https://raw.githubusercontent.com/junegunn/vim-plug/maste
 readonly VIM_PLUG_PATH="$HOME/.local/share/nvim/site/autoload/plug.vim"
 
 if [ ! -f "$VIM_PLUG_PATH" ]; then
-  print_info "INSTALL VIM PLUG…"
-  curl -fLo "$VIM_PLUG_PATH" --create-dirs "$VIM_PLUG_URL" | print_progress
+  info "INSTALL VIM PLUG…"
+  curl -fLo \
+    "$VIM_PLUG_PATH" \
+    --create-dirs \
+    "$VIM_PLUG_URL"
 fi
 
-print_info "INSTALL NEOVIM PYTHON SUPPORT…"
-pip3 install --user neovim | print_progress
+info "INSTALL NEOVIM PYTHON SUPPORT…"
+pip3 install --user neovim
