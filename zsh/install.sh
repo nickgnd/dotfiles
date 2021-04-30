2ff3a61746ab8c155c43051b27ca0e3c73fbcf60
Nicolo Gnudi <nicolo.gnudi@gmail.com>
2021-04-30 12:39:11 +0200

    Update zsh to fit my setup

--- zsh/install.sh
+++ zsh/install.sh
@@ -13,7 +13,7 @@ HOMEBREW_ZSH="$(brew --prefix)/bin/zsh"
 readonly HOMEBREW_ZSH
 
 if [ "$SHELL" != "$HOMEBREW_ZSH" ]; then
-  info "SET HOMEBEW'S ZSH AS DEFAULT SHELL…"
+  info "SET HOMEBREW'S ZSH AS DEFAULT SHELL…"
 
   if ! grep "$HOMEBREW_ZSH" /etc/shells > /dev/null 2>&1 ; then
     sudo sh -c "echo $HOMEBREW_ZSH >> /etc/shells" 2>&1
